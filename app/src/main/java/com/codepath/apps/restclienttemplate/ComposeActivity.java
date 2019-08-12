854ead643c44267c867d066da5300571a2164946
elizabethdumervil@gmail.com <fRIMOMUSSES12>
2019-08-12 15:57:46 -0400

    ComposeActivity added

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,51 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    ImageView ivProfile;
+    TextView tvUsername;
+    EditText etTweet;
+    Button btnCancel;
+    Button btnPost;
+    TextView tvCounter;
+    TwitterClient client;
+    String imageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        // initializes instance variables
+        ivProfile = this.findViewById(R.id.ivProfile);
+        tvUsername = this.findViewById(R.id.tvUsername);
+        etTweet = this.findViewById(R.id.etTweet);
+        btnCancel = this.findViewById(R.id.btnCancel);
+        btnPost = this.findViewById(R.id.btnPost);
+        client = TwitterApp.getRestClient(this);
+    }
+
+    public void onCancel(View view) {
+    }
+
+    // method called when post button is clicked
+    public void composeTweet(android.view.View view) {
+        // grabs body of tweet
+
+            }
+        }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,7 +1,13 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.text.format.DateUtils;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class Tweet {
 
@@ -10,6 +16,8 @@ public class Tweet {
     public long uid; //database ID for the tweet
     public User user;
     public String createAt;
+    public String relativeDate;
+
 
     //deserialize the JSON
     public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
@@ -20,7 +28,28 @@ public class Tweet {
         tweet.uid = jsonObject.getLong("id");
         tweet.createAt = jsonObject.getString("created_at");
         tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        tweet.relativeDate = tweet.getRelativeTimeAgo(tweet.createAt);
         return tweet;
+    }
 
+    // empty constructor needed for parcel
+    public Tweet(){}
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
